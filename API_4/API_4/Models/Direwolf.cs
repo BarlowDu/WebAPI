11b721d09c2b71a1b6d634a3f21cc60a4a47f157
BarlowDu <gangtianci@163.com>
2015-09-06 23:58:24 +0800

    API_4

---
+++ API_4/API_4/Models/Direwolf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_4.Models
+{
+    public class Direwolf
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}
