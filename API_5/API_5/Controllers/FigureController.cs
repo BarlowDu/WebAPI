7b02a031eda0094e59199fbdcef71e4b25feb46b
BarlowDu <gangtianci@163.com>
2015-10-17 19:05:08 +0800

    api6

---
+++ API_5/API_5/Controllers/FigureController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+
+namespace API_5.Controllers
+{
+    public class FigureController : ApiController
+    {
+        HttpActionBinding a = new HttpActionBinding();
+    }
+}
---
+++ API_5/API_5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_5.WebApiApplication" Language="C#" %>
