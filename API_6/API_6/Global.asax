b81e1fb6cf0b3c5e95365f2252d8af9ac8c3e50c
BarlowDu <gangtianci@163.com>
2015-10-17 18:59:15 +0800

    upload

---
+++ API_6/API_6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_6.WebApiApplication" Language="C#" %>
---
+++ API_6/API_6/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace API_6
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configure(t => t.MessageHandlers.Add(new HttpMethodChangeHandler()));
+            //GlobalConfiguration.Configure(t => t.MessageHandlers.Add(new AllBackHttpMessageHandler()));
+            //GlobalConfiguration.DefaultServer
+        }
+    }
+}
