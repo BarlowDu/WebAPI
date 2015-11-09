4295b5133e7390fb0ea23a1b26c66f35bbe9f64d
BarlowDu <gangtianci@163.com>
2015-11-09 22:15:59 +0800

    API11

---
+++ API_11/API_11/Validator/ArrayValidator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using API_11.Models;
+
+namespace API_11.Validator
+{
+    public class ArrayValidator
+    {
+        /// <summary>
+        /// 为CustomerValidationAttribute提供的验证方法（一个参数）
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static ValidationResult Check(object val)
+        {
+            string[] list = new string[] { "Stack", "Other" };
+            if (list.Contains(val))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("不在区间内");
+            }
+        }
+
+        /// <summary>
+        /// 为CustomerValidationAttribute提供的验证方法（两个参数）
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static ValidationResult CheckString(string val, ValidationContext context)
+        {
+            string[] list = new string[] { "Stack", "Other" };
+            if (list.Contains(val))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("不在区间内");
+            }
+        }
+
+    }
+}
